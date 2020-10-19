3fdc74c8ed9fd543660bf3830b0ce6c2f045d19a
davips <dpsabc@gmail.com>
2020-10-19 02:49:40 -0300

    Fix path of modules (context) Reduce space in param-jsons Accumulator as internal Default value for Let

--- kururu/tool/manipulation/let.py
+++ kururu/tool/manipulation/let.py
@@ -3,7 +3,7 @@ from transf.absdata import AbsData
 
 
 class Let(DIStep):
-    def __init__(self, field, value):
+    def __init__(self, field="V", value=True):
         super().__init__({"field": field, "value": value})
 
     def _process_(self, data: AbsData):
