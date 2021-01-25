7d5a2ced577113613d5aff3578b14f5543af8e99
davips <dpsabc@gmail.com>
2021-01-25 17:21:53 -0300

    No memcache

--- kururu/tool/evaluation/mixin/functioninspection.py
+++ kururu/tool/evaluation/mixin/functioninspection.py
@@ -25,12 +25,13 @@ from functools import lru_cache, cached_property
 
 
 class withFunctionInspection:
-    @cached_property
+    ###@cached_property
+    @property
     def function_from_name(self):
         """Map each function name to its corresponding class method."""
         return {name: getattr(self, '_fun_' + name) for name in self.names()}
 
     @classmethod
-    @lru_cache()
+    ###@lru_cache()
     def names(cls):
         return [name.split('_fun_')[1] for name in dir(cls) if '_fun_' in name]
